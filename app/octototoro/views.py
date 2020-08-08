16752b0635733135ef264612445bddb9fd49f5b1
Ilya Isaev <me@ilyaisaev.com>
2020-08-08 13:37:51 +0700

    Remove github token

--- app/octototoro/views.py
+++ app/octototoro/views.py
@@ -7,6 +7,6 @@ def index(request):
 
 
 def repo(request, owner, name):
-    client = GithubClient("fc6ea9740dfbdc2c2b36972d10b77e271664ecb0")
+    client = GithubClient("")
     client.get_teams(owner)
     return render(request, 'repo.html', {'repo': repo, 'prs': {}, 'contributors': {}})
